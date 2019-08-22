6455b0e624d4c5827d54227093baedde1ca3e904
Jim Lacy <lacy@wisc.edu>
2019-08-22 09:58:27 -0500

    Update to Geoblacklight 2.2.1, plus a minor bug fix in how links to static pages are handled.

--- app/assets/javascripts/geoblacklight.js
+++ app/assets/javascripts/geoblacklight.js
@@ -7,7 +7,7 @@
 //= require geoblacklight/downloaders
 //= require leaflet-iiif
 //= require esri-leaflet
-//= require readmore.min
+//= require readmore
 
 // additional leaflet base layers
 GeoBlacklight.Basemaps.esri =  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
