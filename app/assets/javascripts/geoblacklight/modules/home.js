353431c683cc105c80a4e98e9ef84168e5e4d247
Eric Larson <ewlarson@gmail.com>
2019-09-17 15:43:52 -0500

    JS: add custom homepage map
    Add a custom homepage map and remove the default basemap hardcoded minZoom. Fixes #85

---
+++ app/assets/javascripts/geoblacklight/modules/home.js
@@ -0,0 +1,17 @@
+Blacklight.onLoad(function() {
+  $('[data-map="home"]').each(function(i, element) {
+
+    var geoblacklight = new GeoBlacklight.Viewer.Map(this);
+    geoblacklight.map.setMinZoom(6);
+    var data = $(this).data();
+
+    geoblacklight.map.addControl(L.control.geosearch({
+      baseUrl: data.catalogPath,
+      dynamic: false,
+      searcher: function() {
+        window.location.href = this.getSearchUrl();
+      },
+      staticButton: '<a class="btn btn-primary">Search here</a>'
+    }));
+  });
+});
