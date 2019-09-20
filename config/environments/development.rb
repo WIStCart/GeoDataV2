1c4e8463ec20cce8e74a2ef8628b0e08e308558a
Eric Larson <ewlarson@gmail.com>
2019-09-20 14:24:00 -0500

    CSS: reflow to support project heirarchy
    Here we refactor CSS/SCSS files to order dependencies in a base to geodata manner:
    
    1. configuration (bootstrap vars)
    2. bootstrap (ui framework)
    3. blacklight (discovery tool)
    4. geoblacklight (geo discovery tool)
    5. geodata (uw overrides)
    
    Fixes #98

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,14 +50,14 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  # config.assets.debug = true
+  config.assets.debug = true
 
-  # Try production values here
+  # To enable production values, uncomment here
   # Compress JavaScripts and CSS.
-  config.assets.compress = true
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
-  config.assets.debug = false
+  # config.assets.compress = true
+  # config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+  # config.assets.debug = false
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
