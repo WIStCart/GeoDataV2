627a231a2b2889fa23caa7fb98be10bc7e5c2e9c
Eric Larson <ewlarson@gmail.com>
2019-09-25 16:08:18 -0500

    Update admin_mailer.rb
    Add hostname to subject line

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,6 +1,6 @@
 class AdminMailer < ApplicationMailer
   def uri_analysis(report_file_path)
     @report = report_file_path
-    mail(to: 'ewlarson@gmail.com', subject: 'GeoData - URI Analysis Report')
+    mail(to: 'ewlarson@gmail.com', subject: "[GeoData #{`hostname`.strip}] - URI Analysis Report")
   end
 end
