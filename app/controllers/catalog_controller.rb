182864b92c412028a36eb160758556641b596bac
Jim Lacy <lacy@wisc.edu>
2020-04-01 16:30:14 -0500

    adding in dct_temporal on show page for testing

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -131,7 +131,7 @@ class CatalogController < ApplicationController
       '1800s' => { :label => '1800s', :fq => "solr_year_i:[1800 TO 1899]" }
     }, collapse: true
     config.add_facet_field Settings.FIELDS.YEAR, label: 'Year', limit: 10, collapse: false, all: 'Any year', range: {
-      assumed_boundaries: [1100, 2019]
+      assumed_boundaries: [1100, 3000]
     }
     config.add_facet_field Settings.FIELDS.SUBJECT, :label => 'Subject', :limit => 5, collapse: true
     config.add_facet_field Settings.FIELDS.PUBLISHER, :label => 'Publisher', :limit => 8
@@ -179,7 +179,8 @@ class CatalogController < ApplicationController
     config.add_show_field Settings.FIELDS.PUBLISHER, label: 'Publisher', itemprop: 'publisher'
     config.add_show_field Settings.FIELDS.PART_OF, label: 'Collection', itemprop: 'isPartOf', link_to_facet: true
     config.add_show_field Settings.FIELDS.SUBJECT, label: 'Subject(s)', itemprop: 'keywords', link_to_facet: true
-    config.add_show_field Settings.FIELDS.YEAR, label: 'Year', itemprop: 'year'
+    config.add_show_field Settings.FIELDS.TEMPORAL, label: 'dct_temporal', itemprop: 'temporal'
+    config.add_show_field Settings.FIELDS.YEAR, label: 'solr_year', itemprop: 'year'
     config.add_show_field Settings.FIELDS.PROVENANCE, label: 'Held By', link_to_facet: true
     config.add_show_field Settings.FIELDS.SUPPLEMENTAL, label: 'Supplemental Info', itemprop: 'supplemental'
     config.add_show_field(
