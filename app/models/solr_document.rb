3658ca7b98d8247092254841958df47d737b71f7
Eric Larson <ewlarson@gmail.com>
2020-04-16 11:56:03 -0500

    SolrDocument: add WMS rewrite concern

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -2,7 +2,7 @@
 class SolrDocument
   include Blacklight::Solr::Document
   include Geoblacklight::SolrDocument
-  # include WmsRewriteConcern
+  include WmsRewriteConcern
 
   # self.unique_key = 'id'
   self.unique_key = 'layer_slug_s'
