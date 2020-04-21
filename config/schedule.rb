781a8265b4ae75e0ac81bfe9bc886c371effe552
Jim Lacy <lacy@wisc.edu>
2020-04-21 09:11:53 -0500

    Changing cron jobs relative to UTC time

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,24 +22,26 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
+# IMPORTANT!  Our servers are all running on UTC, no timezone is set! 
+# times below are UTC.  Conversion: -5 during CDT, -6 during CST
 # Cleans up recent anonymous search records
-every :day, at: '2:30am', roles: [:app] do
+every :day, at: '8:30am', roles: [:app] do
   rake 'blacklight:delete_old_searches[7]'
 end
 
 # URI analysis
 # At 01:00 on day-of-month 1 and 14.
-every '0 1 1,14 * *', roles: [:app] do
+every '0 7 1,14 * *', roles: [:app] do
   rake 'geodata:uri_purge'
 end
 
 # At 02:00 on day-of-month 1 and 14.
-every '0 2 1,14 * *', roles: [:app] do
+every '0 8 1,14 * *', roles: [:app] do
   rake 'geodata:uri_process_all'
 end
 
 # At 01:00 on day-of-month 2 and 15.
-every '0 1 2,15 * *', roles: [:app] do
+every '0 7 2,15 * *', roles: [:app] do
   rake 'geodata:uri_queue_incomplete_states'
 end
 
@@ -49,6 +51,6 @@ every '0 8 2,15 * *', roles: [:app] do
 end
 
 # Harvest thumbnail images for search results
-every :day, at: '12:05am', roles: [:app] do
+every :day, at: '7:05am', roles: [:app] do
   rake 'gblsci:images:harvest_retry'
 end
